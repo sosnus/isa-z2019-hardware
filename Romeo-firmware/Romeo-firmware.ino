c02a6820dd1cba760d02e4286df794435f557bb4
Sosnus <sosnus15@gmail.com>
2019-11-04 23:18:33 +0100

    print serial plot

--- Romeo-firmware/Romeo-firmware.ino
+++ Romeo-firmware/Romeo-firmware.ino
@@ -1,37 +1,28 @@
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+// #define BATERRY_ADC A0
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+#define  SHARP_L  A1
+#define  SHARP_C  A2
+#define  SHARP_R  A3
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+//enum pins { BATERRY_ADC = A0, SHARP_L = A1, SHARP_L = A2, SHARP_L = A3};
 
-  This example code is in the public domain.
+uint8_t sharp_values[3] = {100,100,100};
 
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  // for (uint8_t i = 0; i < 3; i++)
+  // {
+  //   sharp_values[i] = 4;
+  // }
+  Serial.print(analogRead(SHARP_L));
+  Serial.print("\t");
+  Serial.print(analogRead(SHARP_C));
+  Serial.print("\t");
+  Serial.println(analogRead(SHARP_R));
+delay(50);
 }
